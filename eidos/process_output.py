3108078f50bbced4615308781ba2e7190ddfef60
Ben Gyori <ben.gyori@gmail.com>
2021-03-11 15:40:52 -0500

    Add code for custom grounder

--- eidos/process_output.py
+++ eidos/process_output.py
@@ -3,8 +3,10 @@ import glob
 import json
 import tqdm
 import pickle
+import functools
 from indra.sources import eidos
 from read_pmids import OUTPUT_PATH
+from indra.tools import assemble_corpus as ac
 from indra.assemblers.html import HtmlAssembler
 
 
@@ -15,14 +17,42 @@ def fix_provenance(stmts, pmid):
             ev.text_refs['PMID'] = pmid
 
 
-def process_file(fname):
+def process_file(fname, grounder):
     pmid = os.path.splitext(os.path.basename(fname))[0]
     with open(fname, 'r') as fh:
         jd = json.load(fh)
-    ep = eidos.process_json_bio(json_dict=jd)
+    ep = eidos.process_json_bio(json_dict=jd, grounder=grounder)
     stmts = ep.statements
     fix_provenance(stmts, pmid)
     return stmts
+
+
+def get_custom_grounder():
+    from gilda.grounder import Grounder
+    from gilda.grounder import load_terms_file
+    from gilda.resources import get_grounding_terms
+    from gilda.generate_terms import terms_from_obo_url
+    conso_terms = terms_from_obo_url(
+        'https://raw.githubusercontent.com/pharmacome/conso/master/export/'
+        'conso.obo',
+        prefix='conso')
+    terms = load_terms_file(get_grounding_terms())
+    for term in conso_terms:
+        try:
+            terms[term.norm_text].append(term)
+        except KeyError:
+            terms[term.norm_text] = [term]
+    gr = Grounder(terms)
+    return functools.partial(grounder_wrapper,
+                             grounder=gr)
+
+
+def grounder_wrapper(text, context, grounder):
+    matches = grounder.ground(text, context)
+    if not matches:
+        return {}
+    else:
+        return {matches[0].term.db: matches[0].term.id}
 
 
 if __name__ == '__main__':
@@ -30,11 +60,16 @@ if __name__ == '__main__':
     output_fname = os.path.join(OUTPUT_PATH, f'eidos_{version}.pkl')
     fnames = glob.glob(os.path.join(OUTPUT_PATH, '*.jsonld'))
     all_stmts = []
+    grounder = get_custom_grounder()
     for fname in tqdm.tqdm(fnames):
-        stmts = process_file(fname)
+        stmts = process_file(fname, grounder=grounder)
         all_stmts += stmts
-    with open(output_fname, 'wb') as fh:
-        pickle.dump(all_stmts, fh)
 
-    ha = HtmlAssembler(all_stmts)
+    stmts = ac.map_grounding(all_stmts)
+    stmts = ac.run_preassembly(stmts)
+
+    with open(output_fname, 'wb') as fh:
+        pickle.dump(stmts, fh)
+
+    ha = HtmlAssembler(stmts)
     ha.save_model(os.path.join(OUTPUT_PATH, f'eidos_{version}.html'))
